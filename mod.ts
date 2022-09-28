674882f6ec763a69defb38c5e845e3fd55f7228a
Zaiste <oh@zaiste.net>
2022-09-28 20:15:38 +0200

    refinement: decouple route from handler mapping

--- mod.ts
+++ mod.ts
@@ -1,49 +1,53 @@
-import { graphql, GraphQLSchema } from "graphql";
-import type { Route } from "wren/types.ts";
-import * as Response from "wren/response.ts";
+import { graphql, GraphQLSchema } from 'graphql';
+import type { HandlerMapping, Route } from 'wren';
+import * as Response from 'wren/response.ts';
 
-import type { GraphQLParams } from "./types.ts";
-import { GraphiQL } from "./graphiql.ts";
+import type { GraphQLParams } from './types.ts';
+import { GraphiQL } from './graphiql.ts';
 
 export const performGraphQLOperation = async (
-  schema: GraphQLSchema,
-  params: GraphQLParams,
+	schema: GraphQLSchema,
+	params: GraphQLParams,
 ) => {
-  const { query, mutation, variables: variableValues, operationName } = params;
+	const { query, mutation, variables: variableValues, operationName } = params;
 
-  const result = await graphql({
-    schema,
-    source: (query || mutation)!,
-    variableValues,
-    operationName,
-  });
+	const result = await graphql({
+		schema,
+		source: (query || mutation)!,
+		variableValues,
+		operationName,
+	});
 
-  return result;
+	return result;
 };
 
-export const GraphQL = (
-  pathname: string,
-  schema: GraphQLSchema,
-): Route => {
-  return [pathname, {
-    GET: () => Response.HTML(GraphiQL(pathname)),
-    POST: async (request) => {
-      const { params } = request;
+export const GraphQLHandler = (schema: GraphQLSchema): HandlerMapping => {
+	return {
+		GET: ({ url }) => {
+			const { pathname } = new URL(url);
+			return Response.HTML(GraphiQL(pathname));
+		},
+		POST: async (request) => {
+			const { params } = request;
 
-      try {
-        const result = await performGraphQLOperation(
-          schema,
-          params as unknown as GraphQLParams,
-        );
+			try {
+				const result = await performGraphQLOperation(
+					schema,
+					params as unknown as GraphQLParams,
+				);
 
-        return Response.OK(result);
-      } catch (error) {
-        console.error(error);
+				return Response.OK(result);
+			} catch (error) {
+				console.error(error);
 
-        return Response.BadRequest();
-      }
-    },
-  }];
+				return Response.BadRequest();
+			}
+		},
+	};
 };
 
-export * from "./types.ts";
+export const GraphQL = (pathname: string, schema: GraphQLSchema) => {
+	return [pathname, GraphQLHandler(schema)] as Route;
+};
+
+export * from './types.ts';
--- types.ts
+++ types.ts
@@ -1,18 +1,18 @@
 interface GraphQLOperationParams {
-  variables?: Record<string, unknown>;
-  operationName?: string;
+	variables?: Record<string, unknown>;
+	operationName?: string;
 }
 
 interface GraphQLQueryParams extends GraphQLOperationParams {
-  query: string;
-  mutation?: never;
+	query: string;
+	mutation?: never;
 }
 
 interface GraphQLMutationParams extends GraphQLOperationParams {
-  mutation: string;
-  query?: never;
+	mutation: string;
+	query?: never;
 }
 
 export type GraphQLParams =
-  | GraphQLQueryParams
-  | GraphQLMutationParams;
+	| GraphQLQueryParams
+	| GraphQLMutationParams;
